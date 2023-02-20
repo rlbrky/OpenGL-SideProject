04b376d11e6b47cd4afec6ff8a8c6bc31963d6b2
rlBrky <erol.berkay.cs@gmail.com>
2023-02-20 17:59:11 +0300

    Made rectangle more colorful and tested uniforms.

--- OpenGL-SideProject/src/Main.cpp
+++ OpenGL-SideProject/src/Main.cpp
@@ -117,6 +117,8 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "OLA", NULL, NULL);
+    glfwSwapInterval(1);
+
     if (!window)
     {
         glfwTerminate();
@@ -163,17 +165,31 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
+    //After the shader is bound
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.4f, 0.2f, 0.7f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        
+        GLCall(glUniform4f(location, r, 0.2f, 0.7f, 1.0f));
         //glDrawArrays(GL_TRIANGLES, 0, 3);
                             //The last variable is nullptr because we already binded our buffer in line 132.
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
         
+        if (r > 1.0f)
+            increment = -0.005f;
+        else if (r < 0.0f)
+            increment = 0.005f;
+
+        r += increment;
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
